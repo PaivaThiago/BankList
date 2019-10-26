cf32ab8be3b2c9d462464b68e6842276240ed4da
Thiago Paiva <thiago.paiva@resourceit.com>
2019-10-26 16:49:12 -0300

    First Commit

---
+++ app/src/main/java/paiva/thiago/banklist/MainActivity.kt
@@ -0,0 +1,12 @@
+package paiva.thiago.banklist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
